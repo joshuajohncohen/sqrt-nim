b11b9b8d8d71ea0aa140423e544139c3a2fa09b4
joshuajohncohen <65085812+joshuajohncohen@users.noreply.github.com>
2022-10-03 18:05:04 -0400

    Updated algorithm

--- src/sqrt_calc.nim
+++ src/sqrt_calc.nim
@@ -1,5 +1,4 @@
 import decimal
-import std/math
 
 import std/strformat
 import std/strutils
@@ -7,47 +6,34 @@ import std/strutils
 import std/terminal
 import std/wordwrap
 
+template squared(d: DecimalType): DecimalType =
+  (d * d)
 
-proc sciNotation(n: float64): seq[float64] = 
-  let sciString = &"{n:e}"
-  for x in sciString.split("e"):
-    result.add(float64(parseFloat(x)))
+proc pow(d: DecimalType, e: DecimalType): DecimalType =
+  result = d
 
-proc intSqrt(n: int64): int64 =
-  var float_n = float64(n)
-  var sci_n: seq[float64]
-  for i in sciNotation(float_n):
-    sci_n.add(float64(i))
-  var start = int64(sci_n[0]/10 * (10.pow(floor(sci_n[1] / 2))))
-  var below = float64(start)
-  var above = float64(start)
-  while int64(above.pow(2)) <= int64(float_n):
-    above += 1
-  below = above - 1
-  return int64(below)
+  for _ in 1..parseInt($abs(e)):
+    result *= d
 
-proc squareRoot(n: int64, precision: int64): string =
-  if precision == 0:
-    return $intSqrt(n)
-  elif precision < 0:
-    discard "Error on negative input"
+  if e < newDecimal("0"):
+    result = newDecimal("1") / result
+
+proc squareRoot(n: DecimalType, precision: DecimalType): DecimalType =
+  let dec1 = newDecimal("1")
+  let dec2 = newDecimal("2")
+  let dec10 = newDecimal("10")
   
-  var decimalResult = newDecimal(0)
-  
-  var denominator = int64(10)
-  var numerator = int64(0)
-  var numDigits = int64(0)
+  var numerator = n
+  var numDigits = newDecimal("0")
 
   while numDigits < precision:
-    while (float64(numerator).pow(2) + float64(1)) < float64(float64(n) * float64(denominator).pow(2)):
+    while (numerator + dec1).squared() < n * dec10.pow(numDigits * dec2):
       numerator += 1
 
-    decimalResult += (newDecimal(numerator) / newDecimal(denominator))
-    numDigits += 1
-    numerator = int64(-1)
-    denominator *= int64(10)
+    numerator *= 10
+    numDigits += dec1
 
-  result = $decimalResult
+  result = numerator / dec10.pow(numDigits)
 
 when isMainModule:
   
@@ -58,20 +44,20 @@ when isMainModule:
   echo ""
   
   var
-     num: int64
-     digits: int64
-     answer: string
+     num: DecimalType
+     digits: DecimalType
+     answer: DecimalType
 
   while true:
-    echo "\n\nEnter a positive integer to take the square root of:"
-    num = int64(parseInt(readLine(stdin)))
+    echo "\n\nEnter a positive number to take the square root of:"
+    num = newDecimal(readLine(stdin))
   
     echo "\nWhat precision, in terms of the number of digits following the decimal point, should the result be calculated to? (zero for an integer result)".wrapWords(terminalWidth(), false)
-    digits = int64(parseInt(readLine(stdin)))
+    digits = newDecimal($parseInt(readLine(stdin)))
 
     echo ""
   
     timeSpent "The calculation":
       answer = squareRoot(num, digits)
     
-    echo (&"\n\nThe square root of {num}, to {digits} digit(s) after the decimal point, is {answer}.").wrapWords(terminalWidth(), false)
+    echo (&"\n\nThe square root of {num}, to {digits} digit(s) after the decimal point, is {$answer}.").wrapWords(terminalWidth(), false)
